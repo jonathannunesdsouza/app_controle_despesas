cad86f0705329c7d256278e05e14f6ed3460595c
Jonathan Douglas Nunes de Souza <jonathannunes.souza98@gmail.com>
2021-01-05 21:47:24 -0300

    Criando class Despesa com os atributos ano, mes, dia, tipo, descricao, valor e a instanciando como objeto despesa(let despesa = new Despesa()), recebendo os valores digitados nos campos ano, mes, dia, tipo, descricao, valor.

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
+class Despesa {
+    constructor(ano, mes, dia, tipo, descricao, valor) {
+        this.ano = ano 
+        this.mes = mes
+        this.dia = dia 
+        this.tipo = tipo 
+        this.descricao = descricao 
+        this.valor = valor 
+    }
+}
+
 //função que recupera os valores da view de cadastro de nova despesa
 function cadastrarDespesa() {
    let ano = document.getElementById('ano')
@@ -7,4 +18,16 @@ function cadastrarDespesa() {
    let descricao = document.getElementById('descricao')
    let valor = document.getElementById('valor')
 
+   let despesa = new Despesa(
+    ano.value, 
+    mes.value, 
+    dia.value, 
+    tipo.value, 
+    descricao.value, 
+    valor.value
+   )
+
+   console.log(despesa)
+
 }
+
