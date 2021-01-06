92c9dabcf8864f506c2c47c0a0b85cc78b20f5a5
Jonathan Douglas Nunes de Souza <jonathannunes.souza98@gmail.com>
2021-01-06 07:21:53 -0300

    Criando lógica que insere os valores recuperados na Local Storage de forma dinâmica em tabelas da view de consulta despesas (consulta.html) por meio do método .insertRow() e insertCell().

--- app.js
+++ app.js
@@ -113,6 +113,41 @@ function carregaListaDespesas() {
     
     despesas = bd.recuperarTodosRegistros()
 
-    console.log(despesas)
+    //selecinando o tbody da página consulta.html
+    let listaDespesas = document.getElementById('listaDespesas')
+
+    despesas.forEach(function(d) {
+
+        console.log(d)
+        //criando linha/tr
+        
+        let linha = listaDespesas.insertRow()
+
+        //criando colunas/td
+
+        linha.insertCell(0).innerHTML = `${d.dia}/${d.mes}/ ${d.ano}`
+
+        //ajustar o tipo da despesa
+
+        switch(d.tipo) {
+            case '1': d.tipo = 'Alimentação'
+                break
+            case '2': d.tipo = 'Educação'
+                break
+            case '3': d.tipo = 'Lazer'
+                break
+            case '4': d.tipo = 'Saúde'
+                break
+            case '5': d.tipo = 'Transporte'
+                break
+        }
+
+        linha.insertCell(1).innerHTML = d.tipo
+        linha.insertCell(2).innerHTML = d.descricao
+        linha.insertCell(3).innerHTML = d.valor
+        
+
+
+    })
 }
 
