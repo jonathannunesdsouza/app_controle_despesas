bc576e4857ef805b4ded673b444a79425c869b40
Jonathan Douglas Nunes de Souza <jonathannunes.souza98@gmail.com>
2021-01-06 02:44:40 -0300

    Criando índices dinâmicos com o uso do Local Storage do Browser, p/ que a aplicação cadastre novas despesas de forma dinâmica, sem sobrescrever as despesas anteriormente cadastradas.

--- app.js
+++ app.js
@@ -9,6 +9,32 @@ class Despesa {
     }
 }
 
+class Bd {
+
+    constructor() {
+        let id = localStorage.getItem('id')
+
+        if (id === null) {
+            localStorage.setItem('id', 0)
+        }
+    }
+
+    getProximoId() {
+        let proximoId = localStorage.getItem('id')
+        return (parseInt(proximoId) + 1)
+    }
+
+    gravar(d) {
+        let id = this.getProximoId()
+
+        localStorage.setItem(id, JSON.stringify(d))
+
+        localStorage.setItem('id', id)
+    }    
+}
+
+let bd = new Bd()
+
 //função que recupera os valores da view de cadastro de nova despesa
 function cadastrarDespesa() {
    let ano = document.getElementById('ano')
@@ -27,11 +53,7 @@ function cadastrarDespesa() {
     valor.value
    )
 
-   gravar(despesa)
+   bd.gravar(despesa)
 
-}
-
-function gravar(d) {
-    localStorage.setItem('despesa', JSON.stringify(d))
-}
+} 
 
