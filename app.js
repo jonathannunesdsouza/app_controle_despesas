8ee4c834e8ede5ae80561a802b48de3a2f6a5315
Jonathan Douglas Nunes de Souza <jonathannunes.souza98@gmail.com>
2021-01-07 04:11:30 -0300

    Criando lógica que faz coleta os dados da view de consulta e cria um objeto baseado na classe despesa.

--- app.js
+++ app.js
@@ -61,6 +61,11 @@ class Bd {
 
         return despesas
     }
+    
+    pesquisar(despesa) {
+        console.log(despesa)
+    }
+
 }
 
 let bd = new Bd()
@@ -160,5 +165,18 @@ function carregaListaDespesas() {
 
 
     })
+}
+
+function pesquisarDespesa() {
+    let ano = document.getElementById('ano').value
+    let mes = document.getElementById('mes').value
+    let dia = document.getElementById('dia').value
+    let tipo = document.getElementById('tipo').value
+    let descricao = document.getElementById('descricao').value
+    let valor = document.getElementById('valor').value
+
+    let despesa = new Despesa(ano, mes, dia, tipo, descricao, valor)
+
+    bd.pesquisar(despesa)
 }
 
